6c5520efc9340a8fde6f925e543abeb08ef8ed47
NilsKEA <nallstae@smail.uni-koeln.de>
2023-12-05 16:47:21 +0100

    commit

--- titanic.py
+++ titanic.py
@@ -1,48 +1,56 @@
 import pandas as pd
+import numpy as pd
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense
 
-# read the data from a CSV file (included in the repository)
+# Read the data from a CSV file (included in the repository)
 df = pd.read_csv("data/train.csv")
 
-# ## Step 3
-# 1. Remove the columns "Name" and "PassengerId" (because we know they are irrelevant for our problem).
-df = df.drop("Name", axis=1)
-df = df.drop("PassengerId", axis=1)
+# Data preprocessing steps
+df = df.drop(["Name", "PassengerId"], axis=1)
 
-# 2. Convert all non-numeric columns into numeric ones. The non-numeric columns are "Sex", "Cabin", "Ticket" and "Embarked".
 def make_numeric(df):
-  df["Sex"] = pd.factorize(df["Sex"])[0]
-  df["Cabin"] = pd.factorize(df["Cabin"])[0]
-  df["Ticket"] = pd.factorize(df["Ticket"])[0]
-  df["Embarked"] = pd.factorize(df["Embarked"])[0]
-  return df
-df = make_numeric(df)
+    df["Sex"] = pd.factorize(df["Sex"])[0]
+    df["Cabin"] = pd.factorize(df["Cabin"])[0]
+    df["Ticket"] = pd.factorize(df["Ticket"])[0]
+    df["Embarked"] = pd.factorize(df["Embarked"])[0]
+    return df
 
-# 3. Remove all rows that contain missing values
+df = make_numeric(df)
 df = df.dropna()
 
-# ## Step 4
-# 1. As a next step, we need to split the input features from the training labels. This can be done easily with `pandas`.
+# Split the data into input features and target variable
 y = df["Survived"]
 x = df.drop("Survived", axis=1)
 
-# 2. Secondly, we need to split training and test data. This can be done with the function [`sklearn.model_selection.train_test_split()`](https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html#sklearn.model_selection.train_test_split) from the `scikit-learn` library.
-
-from sklearn.model_selection import train_test_split
-
+# Split the data into training and test sets
 x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0, test_size=0.1)
 
-# 3. Finally, initialize a LogisticRegression object with a `liblinear` solver, and fit it to the training data.
-from sklearn.linear_model import LogisticRegression
+# Define the model
+model = Sequential()
+model.add(Dense(10, input_shape=(x_train.shape[1],), activation='softmax'))
 
-classifier = LogisticRegression(random_state=0, solver="liblinear")
-classifier.fit(x_train, y_train)
+# Compile the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# 4. Lastly, calculate precision/recall/f-score on the test data using the appropriate functions from `scikit-learn`.
-y_pred = classifier.predict(x_test)
+# Train the model
+model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test))
 
+# Evaluate the model
+loss, accuracy = model.evaluate(x_test, y_test)
+print(f"Accuracy: {accuracy}")
+
+# Make predictions
+y_pred = model.predict_step(x_test)
+
+# Evaluate precision, recall, and f1-score
 from sklearn.metrics import precision_score, recall_score, f1_score
 
-print("precision: "+ str(precision_score(y_test, y_pred)))
-print("recall: "+ str(recall_score(y_test, y_pred)))
-print("f1: "+ str(f1_score(y_test, y_pred)))
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
 
+print(f"Precision: {precision}")
+print(f"Recall: {recall}")
+print(f"F1-score: {f1}")
