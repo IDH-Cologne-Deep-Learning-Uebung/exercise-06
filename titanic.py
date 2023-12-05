69fca990b935fd2cb0f9d406f119f9b4ea763b27
msotoma <msotomay@smail.uni-koeln.de>
2023-12-05 23:19:11 +0100

    done# Please enter the commit message for your changes. Lines starting

--- titanic.py
+++ titanic.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
 
 # read the data from a CSV file (included in the repository)
 df = pd.read_csv("data/train.csv")
@@ -25,24 +28,25 @@ df = df.dropna()
 y = df["Survived"]
 x = df.drop("Survived", axis=1)
 
-# 2. Secondly, we need to split training and test data. This can be done with the function [`sklearn.model_selection.train_test_split()`](https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html#sklearn.model_selection.train_test_split) from the `scikit-learn` library.
+#Model 1 with a single hidden layer of size 10
+model = keras.Sequential()
+model.add(layers.Input(shape=(10,)))
+model.add(layers.Dense(10, activation='softmax')
+model.add(layers.Dense(1, activation='softmax')
 
-from sklearn.model_selection import train_test_split
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0, test_size=0.1)
+#Model 2 with 2 hidden layers of sizes 20 and 10
 
-# 3. Finally, initialize a LogisticRegression object with a `liblinear` solver, and fit it to the training data.
-from sklearn.linear_model import LogisticRegression
+model = keras.Sequential()
+model.add(layers.Input(shape=(10,)))
+model.add(layers.Dense(20, activation='sigmoid')
+model.add(layers.Dense(10, activation='relu')
+model.add(layers.Dense(1, activation='softmax')
 
-classifier = LogisticRegression(random_state=0, solver="liblinear")
-classifier.fit(x_train, y_train)
 
-# 4. Lastly, calculate precision/recall/f-score on the test data using the appropriate functions from `scikit-learn`.
-y_pred = classifier.predict(x_test)
+#compile it
+model.compile(loss='mean_square_error', optimizer='sgd', metrics=['accuracy']
 
-from sklearn.metrics import precision_score, recall_score, f1_score
-
-print("precision: "+ str(precision_score(y_test, y_pred)))
-print("recall: "+ str(recall_score(y_test, y_pred)))
-print("f1: "+ str(f1_score(y_test, y_pred)))
+#train it
+model.fit(x_train, y_train, epochs=100, batch_size=5)
 
